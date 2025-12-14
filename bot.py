56ae7acd07413c6a8dc2a31c32aae6311307bba0
vladimirov535 <39128149+vladimirov535@users.noreply.github.com>
2025-12-14 23:28:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -176,7 +176,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # через 15 минут = 15 * 60
     context.job_queue.run_once(
         step_15_job,
-        when=15 * 60,
+        when=20,
         name=job_name(chat_id, "15m"),
         data={"chat_id": chat_id},
     )
